2c1747fd28a08d575de638c4ece2ecc283b9e8e0
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2025-05-07 11:10:32 +0300

    feat: Update styles and layout for statistics section; add new inner container and image

--- src/components/home/Statistics.jsx
+++ src/components/home/Statistics.jsx
@@ -1,7 +1,22 @@
 export default function Statistics() {
   return (
     <section className="statistics_section">
-      
+      <div className="inner_container">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4 col-12">
+              <div className="text">
+                <h6>Our experience</h6>
+                <h2>Leader In Supply Chain Storage & Distribution</h2>
+              </div>
+            </div>
+            <div className="col-lg-4 col-12 ">
+              <img src="/images/ship.png" alt="" />
+            </div>
+            <div className="col-lg-4 col-12"></div>
+          </div>
+        </div>
+      </div>
     </section>
-  )
+  );
 }

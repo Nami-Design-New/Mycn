bdfadfc8fa95c4d678533f01ad3410295a66027c
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2025-05-14 13:46:42 +0300

    Refactor profile styles and enhance MyAddresses and MyTransactions components
    - Moved table and section styles to the bottom of _profile.scss for better organization.
    - Added new button styles for adding addresses in MyAddresses component.
    - Implemented initial address data and display logic in MyAddresses component.
    - Created a transaction history table in MyTransactions component with initial transaction data.

--- src/routes/MyTransactions.jsx
+++ src/routes/MyTransactions.jsx
@@ -1,7 +1,75 @@
 export default function MyTransactions() {
+  const initialData = [
+    {
+      id: 1,
+      date: "2022-10-24",
+      amount: "$100",
+      description: "Order #123",
+      status: "Pending",
+    },
+    {
+      id: 2,
+      date: "2022-10-24",
+      amount: "$100",
+      description: "Order #123",
+      status: "Pending",
+    },
+    {
+      id: 3,
+      date: "2022-10-24",
+      amount: "$100",
+      description: "Order #123",
+      status: "Pending",
+    },
+    {
+      id: 4,
+      date: "2022-10-24",
+      amount: "$100",
+      description: "Order #123",
+      status: "Pending",
+    },
+    {
+      id: 5,
+      date: "2022-10-24",
+      amount: "$100",
+      description: "Order #123",
+      status: "Pending",
+    },
+  ];
   return (
-    <div>
-      
+    <div className="transactions_history">
+      <div className="row">
+        <div className="col-12 p-2">
+          <h3 className="sec_title">My Transactions</h3>
+          <p className="sec_desc">View and manage your transactions history</p>
+        </div>
+
+        <div className="col-12 p-2 mt-2">
+          <table>
+            <thead>
+              <tr>
+                <th>Transaction ID</th>
+                <th>Date</th>
+                <th>Amount</th>
+                <th>Description</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {initialData.map((item) => (
+                <tr key={item.id}>
+                  <td>{item.id}</td>
+                  <td>{item.date}</td>
+                  <td>{item.amount}</td>
+                  <td>{item.description}</td>
+                  <td>{item.status}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
-  )
+  );
 }

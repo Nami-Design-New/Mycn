15830783f6044ece31a2ce917cb07d961d865ca5
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2025-06-26 16:34:53 +0300

    Refactor OrderShipment modal: Replace button with Link for navigation and remove address form

--- src/ui/modals/OrderShipment.jsx
+++ src/ui/modals/OrderShipment.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router";
 import { Form, Modal } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 import SubmitButton from "../forms/SubmitButton";
 import SelectField from "../forms/SelectField";
-import InputField from "../forms/InputField";
 import useGetAddresses from "../../hooks/profile/useGetAddresses";
 import useCreateOrder from "../../hooks/profile/useCreateOrder";
 
@@ -11,7 +11,6 @@ export default function OrderShipment({ show, setShow, consolidated }) {
   const { t } = useTranslation();
   const { data: addresses } = useGetAddresses();
 
-  const [showAddressForm, setShowAddressForm] = useState(false);
   const [payload, setPayload] = useState({
     address_id: "",
     packages_id: [],
@@ -68,73 +67,11 @@ export default function OrderShipment({ show, setShow, consolidated }) {
                   }}
                 />
 
-                <button
-                  onClick={(e) => {
-                    e.preventDefault();
-                    setShowAddressForm(!showAddressForm);
-                  }}
-                >
+                <Link to={"/profile/my-addresses"}>
                   {t("orderModel.addDestination")}
-                </button>
+                </Link>
               </div>
             </div>
-
-            {showAddressForm && (
-              <>
-                <div className="col-12 p-2">
-                  <InputField
-                    label="Add a nickname so you can better manage your addresses*"
-                    placeholder="Enter nickname"
-                  />
-                </div>
-
-                <div className="col-lg-6 col-12 p-2">
-                  <InputField
-                    label="Full Name*"
-                    placeholder="Enter full name"
-                  />
-                </div>
-
-                <div className="col-lg-6 col-12 p-2">
-                  <InputField label="Phone*" placeholder="Enter phone number" />
-                </div>
-
-                <div className="col-lg-6 col-12 p-2">
-                  <InputField
-                    label="Address Line 1*"
-                    placeholder="Enter address line 1"
-                  />
-                </div>
-
-                <div className="col-lg-6 col-12 p-2">
-                  <InputField
-                    label="Address Line 2"
-                    placeholder="Enter address line 2"
-                  />
-                </div>
-
-                <div className="col-lg-6 col-12 p-2">
-                  <SelectField
-                    label="Country*"
-                    defaultSelect="Select a country"
-                    options={[
-                      { value: "Saudi Arabia", name: "Saudi Arabia" },
-                      { value: "Egypt", name: "Egypt" },
-                      {
-                        value: "United Arab Emirates",
-                        name: "United Arab Emirates",
-                      },
-                      { value: "Kuwait", name: "Kuwait" },
-                      { value: "Qatar", name: "Qatar" },
-                    ]}
-                  />
-                </div>
-
-                <div className="col-lg-6 col-12 p-2">
-                  <InputField label="City*" placeholder="Enter city" />
-                </div>
-              </>
-            )}
 
             <div className="col-12 p-2">
               <SubmitButton
